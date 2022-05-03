f7f44ddf3773cb0bb58c9253df4c631d33781e82
shash <shash@LENOVO-FLEX5I>
2022-05-03 19:59:48 +1000

    Add project files.

---
+++ MaxCharCount/CharCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxCharCount
+{
+    class CharCount
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string str = Console.ReadLine();
+            CalcMaxCharCount(str);
+        }
+
+        private static void CalcMaxCharCount(string inputStr)
+        {
+            inputStr = inputStr.ToLower();
+            char[] charArray = inputStr.ToCharArray();
+
+            int[] letterCounts = new int[256];
+            int maxCount = 0;
+            char maxChar = ' ';
+
+            foreach (var r in charArray)
+            {
+                letterCounts[r] = letterCounts[r] + 1;
+                if (maxCount < letterCounts[r])
+                {
+                    maxCount = letterCounts[r];
+                    maxChar = r;
+                }
+            }
+            Console.WriteLine("Character " + maxChar + " has maximum occurences: " + maxCount);
+        }
+
+    }
+}
